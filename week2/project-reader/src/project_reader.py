bfd3738a0bea1619b8e1a1ce60b45c1cafc3c5d5
Tuukka Tuomikoski <tuukka.tuomikoski@gmail.com>
2021-11-15 17:29:15 +0200

    Exercises 1-2

---
+++ week2/project-reader/src/project_reader.py
@@ -0,0 +1,17 @@
+from urllib import request
+from project import Project
+import toml
+
+class ProjectReader:
+    def __init__(self, url):
+        self._url = url
+
+    def get_project(self):
+        # tiedoston merkkijonomuotoinen sisältö
+        content = request.urlopen(self._url).read().decode("utf-8")
+
+        tomlContent = toml.load("../pyproject.toml")
+        dependencies = tomlContent["tool"]["poetry"]
+
+        # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
+        return Project("Test name", "Test description", dependencies["dependencies"], dependencies["dev-dependencies"])
