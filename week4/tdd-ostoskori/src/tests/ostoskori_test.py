ffa6fdafcf67cf92aef2af9c9a50f2f3662a202f
Tuukka Tuomikoski <tuukka.tuomikoski@gmail.com>
2021-11-29 22:01:58 +0200

    Test for two same products added and cart product count

--- week4/tdd-ostoskori/src/tests/ostoskori_test.py
+++ week4/tdd-ostoskori/src/tests/ostoskori_test.py
@@ -34,3 +34,8 @@ class TestOstoskori(unittest.TestCase):
         self.kori.lisaa_tuote(vesi)
         self.assertEqual(self.kori.hinta(),5)
 
+    def test_kahden_saman_tuotteen_lisaaminen_tavaroiden_maara_oikein(self):
+        maito = Tuote("Maito",3)
+        self.kori.lisaa_tuote(maito)
+        self.kori.lisaa_tuote(maito)
+        self.assertEqual(self.kori.tavaroita_korissa(),2)
