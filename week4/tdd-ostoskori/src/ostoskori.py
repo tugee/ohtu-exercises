d03ab99d94031b3e27269f3b309078121216bc78
Tuukka Tuomikoski <tuukka.tuomikoski@gmail.com>
2021-11-29 21:56:55 +0200

    Added tests for cart price

--- week4/tdd-ostoskori/src/ostoskori.py
+++ week4/tdd-ostoskori/src/ostoskori.py
@@ -3,22 +3,23 @@ from ostos import Ostos
 
 class Ostoskori:
     def __init__(self):
-        pass
+        self.tavaroita = 0
+        self._hinta = 0
         # ostoskori tallettaa Ostos-oliota, yhden per korissa oleva Tuote
 
     def tavaroita_korissa(self):
-        return 0
+        return self.tavaroita
         # kertoo korissa olevien tavaroiden lukumäärän
         # eli jos koriin lisätty 2 kpl tuotetta "maito", tulee metodin palauttaa 2 
         # samoin jos korissa on 1 kpl tuotetta "maito" ja 1 kpl tuotetta "juusto", tulee metodin palauttaa 2 
 
     def hinta(self):
-        return 0
+        return self._hinta
         # kertoo korissa olevien ostosten yhteenlasketun hinnan
 
     def lisaa_tuote(self, lisattava: Tuote):
-        # lisää tuotteen
-        pass
+        self.tavaroita += 1
+        self._hinta += lisattava.hinta()
 
     def poista_tuote(self, poistettava: Tuote):
         # poistaa tuotteen
